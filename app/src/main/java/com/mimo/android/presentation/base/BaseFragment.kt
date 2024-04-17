4e4103cb622952a0d3d14f039c75d279207969fb
jaehan4707 <jahan4707@naver.com>
2024-04-17 21:18:09 +0900

    Chore(#1): BaseActivity, BaseFragment 추가

---
+++ app/src/main/java/com/mimo/android/presentation/base/BaseFragment.kt
@@ -0,0 +1,49 @@
+package com.mimo.android.presentation.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+
+abstract class BaseFragment<T : ViewDataBinding>(private val layoutResId: Int) : Fragment() {
+    private var _binding: T? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        initView()
+    }
+
+    abstract fun initView()
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun setupBackStack() {
+        findNavController().popBackStack()
+    }
+
+    fun showMessage(message: String) {
+        Snackbar.make(this.requireView(), message, Snackbar.LENGTH_LONG).show()
+    }
+}
