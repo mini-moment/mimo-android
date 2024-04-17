bfa69bc30483ac8251cccea498734f64086ff9e5
jaehan4707 <jahan4707@naver.com>
2024-04-17 21:18:30 +0900

    Chore(#1): MainActivity BaseActivity 적용

---
+++ app/src/main/java/com/mimo/android/presentation/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mimo.android.presentation
+
+import com.mimo.android.R
+import com.mimo.android.databinding.ActivityMainBinding
+import com.mimo.android.presentation.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+        //UI 관련 초기화 작업
+    }
+}
