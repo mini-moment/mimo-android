c6a70b9d6ad0e27da6ee8d593af03035abe68082
ho-taek <kht072623@naver.com>
2024-04-22 22:12:41 +0900

    feat : 바텀네비게이션 및 프래그먼트 세팅
    MapFragment, MyPageFragment 세팅과 바텀네비게이션 및 menu 세팅

---
+++ app/src/main/java/com/mimo/android/presentation/map/MapFragment.kt
@@ -0,0 +1,18 @@
+package com.mimo.android.presentation.map
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mimo.android.R
+import com.mimo.android.databinding.FragmentMapBinding
+import com.mimo.android.presentation.base.BaseFragment
+
+
+class MapFragment : BaseFragment<FragmentMapBinding>(R.layout.fragment_map) {
+
+    override fun initView() {
+
+    }
+}
---
+++ app/src/main/java/com/mimo/android/presentation/mypage/MyPageFragment.kt
@@ -0,0 +1,13 @@
+package com.mimo.android.presentation.mypage
+
+import com.mimo.android.R
+import com.mimo.android.databinding.FragmentMyPageBinding
+import com.mimo.android.presentation.base.BaseFragment
+
+
+class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+    override fun initView() {
+
+    }
+}
