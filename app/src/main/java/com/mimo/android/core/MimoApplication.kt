c1432c6e93a94a550f6cf025e43dc7f899230f66
ho-taek <kht072623@naver.com>
2024-04-23 19:58:07 +0900

    feat: 코드 정리

--- app/src/main/java/com/mimo/android/core/MimoApplication.kt
+++ app/src/main/java/com/mimo/android/core/MimoApplication.kt
@@ -4,10 +4,8 @@ import android.app.Application
 import timber.log.Timber
 
 class MimoApplication : Application() {
-
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
-
     }
 }
--- app/src/main/java/com/mimo/android/presentation/map/MapFragment.kt
+++ app/src/main/java/com/mimo/android/presentation/map/MapFragment.kt
@@ -1,10 +1,5 @@
 package com.mimo.android.presentation.map
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.mimo.android.R
 import com.mimo.android.databinding.FragmentMapBinding
 import com.mimo.android.presentation.base.BaseFragment
