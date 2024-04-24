8bc23d1c8601150bfd997511e79a2cfea1e1efcc
jaehan4707 <jahan4707@naver.com>
2024-04-25 01:29:36 +0900

    feat(#12): SplashViewModel 구현

---
+++ app/src/main/java/com/mimo/android/presentation/splash/SplashViewModel.kt
@@ -0,0 +1,40 @@
+package com.mimo.android.presentation.splash
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mimo.android.data.repository.DataStoreRepository
+import com.mimo.android.data.response.ApiResponse
+import com.mimo.android.presentation.login.LoginEvent
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class SplashViewModel(private val dataStoreRepository: DataStoreRepository) : ViewModel() {
+    private val _event = MutableSharedFlow<LoginEvent>()
+    val event: SharedFlow<LoginEvent> = _event
+
+    init {
+        getUserPreferences()
+    }
+
+    private fun getUserPreferences() {
+        viewModelScope.launch {
+            dataStoreRepository.getAccessToken().collectLatest { loginResponse ->
+                when (loginResponse) {
+                    is ApiResponse.Success -> _event.emit(LoginEvent.Success)
+                    is ApiResponse.Error -> _event.emit(
+                        LoginEvent.Error(
+                            errorCode = loginResponse.errorCode,
+                            errorMessage = loginResponse.errorMessage,
+                        ),
+                    )
+
+                    is ApiResponse.Failure -> {
+                        _event.emit(LoginEvent.Error())
+                    }
+                }
+            }
+        }
+    }
+}
