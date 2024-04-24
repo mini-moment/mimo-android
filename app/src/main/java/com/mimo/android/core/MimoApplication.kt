d68c5039de8b06123aefe50a692e90b536c23443
jaehan4707 <jahan4707@naver.com>
2024-04-25 01:26:39 +0900

    feat(#12): ApplicationClass 수정
    dataStore 추가 및 NaverLoginSdk 초기화

--- app/src/main/java/com/mimo/android/core/MimoApplication.kt
+++ app/src/main/java/com/mimo/android/core/MimoApplication.kt
@@ -1,11 +1,26 @@
 package com.mimo.android.core
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.mimo.android.BuildConfig
+import com.navercorp.nid.NaverIdLoginSDK
 import timber.log.Timber
 
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = BuildConfig.DATASTORE_NAME)
+
 class MimoApplication : Application() {
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        NaverIdLoginSDK.initialize(
+            this,
+            BuildConfig.CLIENT_ID,
+            BuildConfig.CLIENT_SECRET,
+            "이재한",
+        )
     }
 }
