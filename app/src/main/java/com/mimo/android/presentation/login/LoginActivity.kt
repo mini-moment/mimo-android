20e09e606af8957efcad5611b201d8c5f4f0a63e
jaehan4707 <jahan4707@naver.com>
2024-04-25 01:30:29 +0900

    feat(#12): LoginActivity 로그인 처리

--- app/src/main/java/com/mimo/android/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/mimo/android/presentation/login/LoginActivity.kt
@@ -1,11 +1,51 @@
 package com.mimo.android.presentation.login
 
+import android.content.Intent
+import androidx.lifecycle.lifecycleScope
 import com.mimo.android.R
+import com.mimo.android.core.dataStore
+import com.mimo.android.data.network.login.NaverLoginManager
+import com.mimo.android.data.repository.DataStoreRepository
 import com.mimo.android.databinding.ActivityLoginBinding
+import com.mimo.android.presentation.MainActivity
 import com.mimo.android.presentation.base.BaseActivity
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    private lateinit var dataStoreRepository: DataStoreRepository
+    private lateinit var loginViewModel: LoginViewModel
+
     override fun init() {
-        //UI 관련 작업
+        setDataStoreRepository()
+        collectLoginEvent()
+        with(binding) {
+            btnNaverLogin.setOnClickListener {
+                NaverLoginManager.login(this@LoginActivity)
+            }
+        }
+    }
+
+    private fun setDataStoreRepository() {
+        dataStoreRepository = DataStoreRepository(application.dataStore)
+        loginViewModel = LoginViewModel(dataStoreRepository)
+    }
+
+    private fun collectLoginEvent() {
+        lifecycleScope.launch {
+            loginViewModel.event.collectLatest { loginEvent ->
+                when (loginEvent) {
+                    is LoginEvent.Success -> {
+                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+
+                    is LoginEvent.Error -> {
+                        showMessage(loginEvent.errorMessage)
+                    }
+                }
+            }
+        }
     }
 }
