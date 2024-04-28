b1e2e1a3ca7d4fefe34a428fb4cde6c3f9716789
jaehan4707 <jahan4707@naver.com>
2024-04-28 21:21:52 +0900

    feat(#18): LoginResponse 수정

--- app/src/main/java/com/mimo/android/data/response/LoginResponse.kt
+++ app/src/main/java/com/mimo/android/data/response/LoginResponse.kt
@@ -1,6 +1,8 @@
 package com.mimo.android.data.response
 
 data class LoginResponse(
+    val userName: String = "",
+    val userContract: String = "",
     val accessToken: String = "",
     val refreshToken: String = "",
 )
