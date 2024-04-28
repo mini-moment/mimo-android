0f538ae3009843315863b63626d5a47c67866500
jaehan4707 <jahan4707@naver.com>
2024-04-28 21:26:01 +0900

    feat(#18): ProfileCallback 추가

--- app/src/main/java/com/mimo/android/data/network/login/NaverLoginManager.kt
+++ app/src/main/java/com/mimo/android/data/network/login/NaverLoginManager.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import com.mimo.android.data.response.ApiResponse
 import com.mimo.android.data.response.LoginResponse
 import com.navercorp.nid.NaverIdLoginSDK
+import com.navercorp.nid.oauth.NidOAuthLogin
 import com.navercorp.nid.oauth.OAuthLoginCallback
+import com.navercorp.nid.profile.NidProfileCallback
+import com.navercorp.nid.profile.data.NidProfileResponse
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
@@ -13,14 +16,32 @@ object NaverLoginManager {
     private val _loginResult = MutableStateFlow<ApiResponse<LoginResponse>>(ApiResponse.Failure)
     val loginResult: StateFlow<ApiResponse<LoginResponse>> = _loginResult
 
-    private val oauthLoginCallback = object : OAuthLoginCallback {
-        override fun onSuccess() {
+    private val profileCallback = object : NidProfileCallback<NidProfileResponse> {
+        override fun onSuccess(response: NidProfileResponse) {
             _loginResult.value = ApiResponse.Success(
                 LoginResponse(
+                    userName = response.profile?.name ?: "",
+                    userContract = response.profile?.mobile ?: "",
                     accessToken = NaverIdLoginSDK.getAccessToken() ?: "",
                     refreshToken = NaverIdLoginSDK.getRefreshToken() ?: "",
                 ),
             )
+        }
+
+        override fun onFailure(httpStatus: Int, message: String) {
+            _loginResult.value = ApiResponse.Error(
+                errorCode = httpStatus,
+                errorMessage = message,
+            )
+        }
+
+        override fun onError(errorCode: Int, message: String) {
+            onFailure(errorCode, message)
+        }
+    }
+    private val oauthLoginCallback = object : OAuthLoginCallback {
+        override fun onSuccess() {
+            NidOAuthLogin().callProfileApi(profileCallback)
         }
 
         override fun onFailure(httpStatus: Int, message: String) {
