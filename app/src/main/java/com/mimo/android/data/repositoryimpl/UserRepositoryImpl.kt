d73b6eb763f8c1c9e4d20c4f2619a5d4dbae484c
jaehan4707 <jahan4707@naver.com>
2024-05-01 21:09:52 +0900

    feat(#23): 결과 처리 로직 변경

--- app/src/main/java/com/mimo/android/data/repositoryimpl/UserRepositoryImpl.kt
+++ app/src/main/java/com/mimo/android/data/repositoryimpl/UserRepositoryImpl.kt
@@ -1,11 +1,11 @@
 package com.mimo.android.data.repositoryimpl
 
-import com.mimo.android.data.datasource.local.LocalDataSource
 import com.mimo.android.data.datasource.remote.UserRemoteDataSource
 import com.mimo.android.data.repository.DataStoreRepository
 import com.mimo.android.data.repository.UserRepository
-import com.mimo.android.data.request.UserRequest
-import com.mimo.android.data.response.ApiResponse
+import com.mimo.android.data.model.request.UserRequest
+import com.mimo.android.data.model.response.ApiResponse
+import com.mimo.android.util.ErrorCode
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -17,18 +17,20 @@ class UserRepositoryImpl @Inject constructor(
     override fun signUp(userRequest: UserRequest): Flow<ApiResponse<Boolean>> = flow {
         runCatching {
             userRemoteDataSource.signUp(userRequest)
-        }.onSuccess {
-            if (it.isSuccessful) {
-                dataStoreRepository.saveAccessToken(userRequest.accessToken)
-                dataStoreRepository.saveRefreshToken(userRequest.refreshToken)
-                emit(ApiResponse.Success(data = true))
-            } else { // 네트워크 요청의 실패 다른 에러코드
-                emit(
-                    ApiResponse.Error(
-                        errorCode = it.code(),
-                        errorMessage = it.message(),
-                    ),
-                )
+        }.onSuccess { response ->
+            response.body()?.let {
+                it.data?.let {
+                    dataStoreRepository.saveAccessToken(userRequest.accessToken)
+                    dataStoreRepository.saveRefreshToken(userRequest.refreshToken)
+                    emit(ApiResponse.Success(data = it))
+                } ?: run {
+                    emit(
+                        ApiResponse.Error(
+                            errorCode = it.statusCode ?: ErrorCode.NONE,
+                            errorMessage = it.message ?: "",
+                        ),
+                    )
+                }
             }
         }.onFailure { // 다른 예외가 발생한 경우 -> 서버가 닫힌 경우
             emit(ApiResponse.Error(errorMessage = it.message ?: ""))
--- app/src/main/java/com/mimo/android/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/mimo/android/presentation/login/LoginViewModel.kt
@@ -2,11 +2,11 @@ package com.mimo.android.presentation.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mimo.android.data.model.request.UserRequest
+import com.mimo.android.data.model.response.ApiResponse
+import com.mimo.android.data.model.response.LoginResponse
 import com.mimo.android.data.network.login.NaverLoginManager
 import com.mimo.android.data.repository.UserRepository
-import com.mimo.android.data.request.UserRequest
-import com.mimo.android.data.response.ApiResponse
-import com.mimo.android.data.response.LoginResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
@@ -52,10 +52,10 @@ class LoginViewModel @Inject constructor(
     private suspend fun userSignUp(loginResponse: LoginResponse) {
         userRepository.signUp(
             UserRequest(
-                userName = loginResponse.userName,
-                userContract = loginResponse.userContract,
-                accessToken = loginResponse.accessToken,
-                refreshToken = loginResponse.refreshToken,
+                userName = loginResponse.userName ?: "",
+                userContact = loginResponse.userContact ?: "",
+                accessToken = loginResponse.accessToken ?: "",
+                refreshToken = loginResponse.refreshToken ?: "",
             ),
         ).collectLatest { signUpResponse ->
             when (signUpResponse) {
