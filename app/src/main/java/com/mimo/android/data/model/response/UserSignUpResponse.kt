9fa7f11d2f49b7d336737e9a6ae14416a6116a2a
jaehan4707 <jahan4707@naver.com>
2024-05-02 21:15:58 +0900

    feat(#24): CustomErrorCode 제거

--- app/src/main/java/com/mimo/android/data/model/response/UserSignUpResponse.kt
+++ app/src/main/java/com/mimo/android/data/model/response/UserSignUpResponse.kt
@@ -1,7 +1,5 @@
 package com.mimo.android.data.model.response
 
-import com.mimo.android.util.ErrorCode
-
 data class UserSignUpResponse(
     val data: Boolean? = null,
     val statusCode: String? = null,
--- app/src/main/java/com/mimo/android/presentation/login/LoginEvent.kt
+++ app/src/main/java/com/mimo/android/presentation/login/LoginEvent.kt
@@ -1,7 +1,5 @@
 package com.mimo.android.presentation.login
 
-import com.mimo.android.util.ErrorCode
-
 
 sealed interface LoginEvent {
     data object Success : LoginEvent
