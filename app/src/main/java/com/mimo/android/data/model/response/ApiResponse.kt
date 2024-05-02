a58bf2d90045387a4fe0d79819127584870788c4
jaehan4707 <jahan4707@naver.com>
2024-05-02 21:16:20 +0900

    feat(#24): apiHandler 구현

--- app/src/main/java/com/mimo/android/data/model/response/ApiResponse.kt
+++ app/src/main/java/com/mimo/android/data/model/response/ApiResponse.kt
@@ -1,6 +1,6 @@
 package com.mimo.android.data.model.response
 
-import com.mimo.android.util.ErrorCode
+import retrofit2.Response
 
 sealed class ApiResponse<out T : Any?> {
     data class Success<out T : Any?>(
@@ -15,3 +15,24 @@ sealed class ApiResponse<out T : Any?> {
     data object Failure : ApiResponse<Nothing>()
 }
 
+suspend fun <T> apiHandler(
+    apiResponse: suspend () -> Pair<Response<T>, ErrorResponse?>,
+): ApiResponse<T> {
+    runCatching {
+        val action = apiResponse.invoke()
+        val response = action.first
+        if (response.isSuccessful) {
+            response.body()?.let { body ->
+                return ApiResponse.Success(body)
+            }
+        } else {
+            return ApiResponse.Error(
+                errorCode = action.second?.statusCode ?: 0,
+                errorMessage = action.second?.message ?: "",
+            )
+        }
+    }.onFailure {
+        return ApiResponse.Error(errorMessage = it.message ?: "")
+    }
+    return ApiResponse.Failure
+}
