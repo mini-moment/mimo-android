21aa94f1b851501016f1a14c109c50ffd36e88ef
jaehan4707 <jahan4707@naver.com>
2024-05-08 22:08:49 +0900

    feat(#35): TagRepository 구현

--- app/src/main/java/com/mimo/android/core/di/RepositoryModule.kt
+++ app/src/main/java/com/mimo/android/core/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package com.mimo.android.core.di
 
 import com.mimo.android.data.repository.DataStoreRepository
+import com.mimo.android.data.repository.TagRepository
 import com.mimo.android.data.repository.UserRepository
 import com.mimo.android.data.repositoryimpl.DataStoreRepositoryImpl
+import com.mimo.android.data.repositoryimpl.TagRepositoryImpl
 import com.mimo.android.data.repositoryimpl.UserRepositoryImpl
 import dagger.Binds
 import dagger.Module
@@ -25,4 +27,10 @@ interface RepositoryModule {
     fun provideUserRepository(
         userRepositoryImpl: UserRepositoryImpl,
     ): UserRepository
+
+    @Singleton
+    @Binds
+    fun provideTagRepository(
+        tagRepositoryImpl: TagRepositoryImpl,
+    ): TagRepository
 }
