0a374a553626a6636db22205602d506e9dd432fd
jaehan4707 <jahan4707@naver.com>
2024-05-11 22:54:40 +0900

    feat(#38): ErrorMessage 추가
    영상, 주제 없을 경우

--- app/src/main/java/com/mimo/android/presentation/util/ErrorMessage.kt
+++ app/src/main/java/com/mimo/android/presentation/util/ErrorMessage.kt
@@ -4,4 +4,6 @@ object ErrorMessage {
     const val NO_ACCESS_TOKEN_MESSAGE = "No Access Token"
     const val NO_REFRESH_TOKEN_MESSAGE = "No Refresh Token"
     const val ENOUGH_HASH_TAG_MESSAGE = "해시태그는 최대 5개 지정 가능합니다."
+    const val NO_POST_VIDEO_URL = "영상을 선택해주세요."
+    const val NO_POST_TOPIC = "주제를 입력해주세요."
 }
