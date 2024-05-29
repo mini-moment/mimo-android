7b150ffb8e5604069c0e6faf4146878046da6505
jaehan4707 <jahan4707@naver.com>
2024-05-29 17:16:16 +0900

    feat(#48): UiState thumbnails 추가, thumbnails 관련 이벤트 추가

--- app/src/main/java/com/mimo/android/presentation/video/upload/UploadVideoEvent.kt
+++ app/src/main/java/com/mimo/android/presentation/video/upload/UploadVideoEvent.kt
@@ -5,6 +5,8 @@ interface UploadVideoEvent {
     data object VideoUploadSuccess : UploadVideoEvent
     data object PostUploadSuccess : UploadVideoEvent
 
+    data object ThumbnailsGetSuccess : UploadVideoEvent
+
     data class Error(
         val errorCode: Int = 0,
         val errorMessage: String = "",
--- app/src/main/java/com/mimo/android/presentation/video/upload/UploadVideoUiState.kt
+++ app/src/main/java/com/mimo/android/presentation/video/upload/UploadVideoUiState.kt
@@ -3,8 +3,9 @@ package com.mimo.android.presentation.video.upload
 import com.mimo.android.domain.model.TagData
 
 data class UploadVideoUiState(
-    val videoUri: String? = null,
+    val videoUri: String = "",
     val tags: List<TagData> = listOf(),
-    val topic: String? = null,
+    val topic: String = "",
     val selectedTags: List<TagData> = listOf(),
+    val thumbnails: List<VideoThumbnail> = listOf(),
 )
