f37293bb7c5733da6be41ea627ee69239ca79a6f
ho-taek <kht072623@naver.com>
2024-06-11 14:16:51 +0900

    feat(#54) : 다크모드 막기

--- app/src/main/java/com/mimo/android/core/MimoApplication.kt
+++ app/src/main/java/com/mimo/android/core/MimoApplication.kt
@@ -1,6 +1,7 @@
 package com.mimo.android.core
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.mimo.android.BuildConfig
 import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.HiltAndroidApp
@@ -11,6 +12,7 @@ class MimoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         NaverIdLoginSDK.initialize(
             this,
             BuildConfig.CLIENT_ID,
