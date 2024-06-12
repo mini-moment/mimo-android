17ee55e022db20fd648479e73c9bcf7b09f8aa8a
jaehan4707 <jahan4707@naver.com>
2024-06-12 12:34:07 +0900

    feat(#58): profileImageUrl 파라미터 추가

--- app/src/main/java/com/mimo/android/data/model/response/LoginResponse.kt
+++ app/src/main/java/com/mimo/android/data/model/response/LoginResponse.kt
@@ -3,6 +3,7 @@ package com.mimo.android.data.model.response
 data class LoginResponse(
     val userName: String? = null,
     val userContact: String? = null,
+    val profileImageUrl: String? = null,
     val accessToken: String? = null,
     val refreshToken: String? = null,
 )
