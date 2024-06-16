a61a96db12a4eef4bf2fb64f56917c794c860256
ho-taek <kht072623@naver.com>
2024-06-16 17:02:44 +0900

    ui(#61) : 마이페이지 ui 완료

---
+++ app/src/main/java/com/mimo/android/presentation/mypage/adapter/MyPostAdapter.kt
@@ -0,0 +1,38 @@
+package com.mimo.android.presentation.mypage.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.mimo.android.databinding.ItemMyPostBinding
+import com.mimo.android.domain.model.PostData
+import com.mimo.android.presentation.util.DiffUtilCallback
+import com.mimo.android.presentation.video.upload.TagListAdapter
+
+class MyPostAdapter : ListAdapter<PostData, MyPostAdapter.MyPostViewHolder>(
+    DiffUtilCallback<PostData>()
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyPostViewHolder {
+        val binding = ItemMyPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyPostViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyPostViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+
+    class MyPostViewHolder(
+        private val binding: ItemMyPostBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(postData: PostData) {
+            binding.apply {
+                val tagListAdapter = TagListAdapter()
+                rcTagList.adapter = tagListAdapter
+                tagListAdapter.submitList(postData.tagList)
+
+            }
+        }
+    }
+}
