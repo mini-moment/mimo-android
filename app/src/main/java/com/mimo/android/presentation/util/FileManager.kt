20727a9ca8a6f3e6acaeb2e1faa38aa5d6b82bf6
jaehan4707 <jahan4707@naver.com>
2024-06-16 16:24:48 +0900

    feat(#60): convertBitmapToFile 구현

--- app/src/main/java/com/mimo/android/presentation/util/FileManager.kt
+++ app/src/main/java/com/mimo/android/presentation/util/FileManager.kt
@@ -1,8 +1,13 @@
 package com.mimo.android.presentation.util
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.net.Uri
+import android.os.Environment
 import android.provider.MediaStore
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 fun getRealPathFromURI(context: Context, contentUri: Uri): String {
     val projection = arrayOf(MediaStore.Images.Media.DATA)
@@ -12,4 +17,22 @@ fun getRealPathFromURI(context: Context, contentUri: Uri): String {
     val filePath = cursor?.getString(columnIndex!!)
     cursor?.close()
     return filePath!!
+}
+
+fun convertBitmapToFile(bitmap: Bitmap): File {
+    val rootPath =
+        Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+            .toString()
+    val file = File(
+        "$rootPath/clip.jpeg",
+    )
+    try {
+        FileOutputStream(file).use { out ->
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
+            out.flush()
+        }
+    } catch (e: IOException) {
+        e.printStackTrace()
+    }
+    return file
 }
