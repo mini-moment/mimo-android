9bbcde050b117dc382801dd3b6665d5bd878c003
jaehan4707 <jahan4707@naver.com>
2024-07-01 22:08:15 +0900

    feat(#77) : MarkerEvent LongClickMarker 추가

--- app/src/main/java/com/mimo/android/presentation/map/MarkerEvent.kt
+++ app/src/main/java/com/mimo/android/presentation/map/MarkerEvent.kt
@@ -4,7 +4,15 @@ interface MarkerEvent {
 
     data class LeafMarker(val idx: Int) : MarkerEvent
 
-    data class ClusterMarker(val idxList: List<Int>,val latitude : Double, val longitude : Double) : MarkerEvent
+    data class ClusterMarker(
+        val idxList: List<Int>,
+        val latitude: Double,
+        val longitude: Double,
+    ) : MarkerEvent
 
-
+    data class LongClickMarker(
+        val latitude: Double,
+        val longitude: Double,
+        val address: String,
+    ) : MarkerEvent
 }
