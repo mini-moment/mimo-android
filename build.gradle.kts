ec40795c7f824b7776daff4a303b1ad3951cb42c
jaehan4707 <jahan4707@naver.com>
2024-07-01 22:16:47 +0900

    style : 코드 정렬

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,5 @@ buildscript {
     dependencies {
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-
     }
 }
