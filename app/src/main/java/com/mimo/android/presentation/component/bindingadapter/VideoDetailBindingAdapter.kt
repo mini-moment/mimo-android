5126945609e0c1335c0811969d1c94dd436b658c
jaehan4707 <jahan4707@naver.com>
2024-07-05 22:36:00 +0900

    ui(#84): 프로필 이미지 잘림 버그 해결

--- app/src/main/java/com/mimo/android/presentation/component/bindingadapter/VideoDetailBindingAdapter.kt
+++ app/src/main/java/com/mimo/android/presentation/component/bindingadapter/VideoDetailBindingAdapter.kt
@@ -1,6 +1,7 @@
 package com.mimo.android.presentation.component.bindingadapter
 
 import android.widget.ImageView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
@@ -21,6 +22,9 @@ fun ViewPager2.bindPosts(items: List<PostData>) {
 fun ImageView.bindUserProfileImage(url: String) {
     Glide.with(context)
         .load(url)
+        .centerCrop()
+        .circleCrop()
+        .placeholder(ContextCompat.getDrawable(this.context, R.drawable.ic_user_selected))
         .into(this)
 }
 
@@ -30,6 +34,7 @@ fun ChipGroup.bindPostHashTags(tagList: List<PostData.Tag>) {
     tagList.forEach { tag ->
         Chip(context).apply {
             text = tag.name
+            letterSpacing = (-0.07).toFloat()
             setChipIconResource(R.drawable.ic_hash_tag)
             chipIconSize = 30f
             setChipBackgroundColorResource(R.color.splash_logo_message_color)
