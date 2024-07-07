b8470fe1828adf5023026584e730cc92787f798c
jaehan4707 <jahan4707@naver.com>
2024-07-07 18:30:08 +0900

    feat(#90): MarkerData Naver, Android 의존성 제거 Serializable 추가

--- app/src/main/java/com/mimo/android/domain/model/MarkerData.kt
+++ app/src/main/java/com/mimo/android/domain/model/MarkerData.kt
@@ -1,28 +1,11 @@
 package com.mimo.android.domain.model
 
-import android.os.Parcelable
-import com.mimo.android.data.model.response.MarkerResponse
-import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.clustering.ClusteringKey
-import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
-@Parcelize
+@Serializable
 data class MarkerData(
     val id: Int,
     val latitude: Double,
     val longitude: Double,
     val postId: Int,
-) : ClusteringKey, Parcelable {
-    override fun getPosition(): LatLng = LatLng(latitude, longitude)
-}
-
-fun MarkerResponse.toMarkerData(): List<MarkerData> {
-    return this.data.map {
-        MarkerData(
-            id = it.id,
-            latitude = it.latitude,
-            longitude = it.longitude,
-            postId = it.postId,
-        )
-    }
-}
+)
