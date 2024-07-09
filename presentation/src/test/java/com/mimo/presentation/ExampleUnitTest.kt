00fc2d2780799c3fd074a2b0534968b6acaa71b8
jaehan4707 <jahan4707@naver.com>
2024-07-10 02:30:04 +0900

    chore(#92): naver.oauth.aar 추가,

---
+++ presentation/src/test/java/com/mimo/presentation/ExampleUnitTest.kt
@@ -0,0 +1,18 @@
+package com.mimo.presentation
+
+import org.junit.Assert.*
+import org.junit.Test
+
+// ktlint-disable no-wildcard-imports
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
