f3e3b9c5e51a0b521d5b054f318e3928d44dd803
jaehan4707 <jahan4707@naver.com>
2024-07-09 21:41:56 +0900

    chore(#92): .gitignore, AndroidManifest, ErrorMessage, Test 추가

---
+++ data/src/main/java/com/mimo/data/util/ErrorMessage.kt
@@ -0,0 +1,12 @@
+package com.mimo.data.util
+
+object ErrorMessage {
+    const val NO_ACCESS_TOKEN_MESSAGE = "No Access Token"
+    const val NO_REFRESH_TOKEN_MESSAGE = "No Refresh Token"
+    const val ENOUGH_HASH_TAG_MESSAGE = "해시태그는 최대 6개 지정 가능합니다."
+    const val NO_POST_VIDEO_URL = "영상을 선택해주세요."
+    const val NO_POST_TOPIC = "주제를 입력해주세요."
+    const val GET_THUMBNAILS_ERROR_MESSAGE = "썸네일을 불러오는데 실패했습니다."
+    const val FILE_SIZE_EXCEEDED_MESSAGE = "파일 용량이 초과되었습니다."
+    const val GPS_ERROR_MESSAGE = "위치정보를 불러오는데 실패했습니다."
+}
